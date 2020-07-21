cf435f68b8398674151fc04a32333955802498a5
Alfred Varghese Jose <alfredzeee@gmail.com>
2020-07-22 00:41:09 +0530

    Making some major corrections (#7)

--- cms/apps/home/urls.py
+++ cms/apps/home/urls.py
@@ -14,13 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('home/', include('apps.home.urls')),
-    path('attendance/', include('apps.attendance.urls')),
-    path('internal/', include('apps.internal.urls')),
-    path('activitypoint/', include('apps.activitypoint.urls')),
-    path('userprofile/', include('apps.userprofile.urls')),
+
 ]
