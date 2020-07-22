ca4b47ea4b9045c771c048121f2b543198a7935e
Alfred V Jose <alfredzeee@gmail.com>
2020-07-22 13:55:48 +0530

    Corrections in authentication

--- cms/apps/authentication/models.py
+++ cms/apps/authentication/models.py
@@ -17,8 +17,8 @@ class Classroom(models.Model):
 
 class User(AbstractUser):
     type = models.CharField(max_length = 20, choices = TYPE_CHOICES , default = 'Student')
-    rollno = models.CharField(max_length = 50, null = True)
-    class_name = models.ForeignKey(Classroom, on_delete = models.CASCADE,null = True)
+    rollno = models.CharField(max_length = 50, null = True, blank = True)
+    class_name = models.ForeignKey(Classroom, on_delete = models.CASCADE,null = True, blank = True)
 
     def __str__(self):
         return '%s %s' % (self.first_name,self.last_name)
@@ -28,5 +28,5 @@ class Subject(models.Model):
     subject_name = models.CharField(max_length = 100)
     subject_code = models.CharField(max_length = 20)
 
-    def __self__(self):
+    def __str__(self):
         return '%s : %s' % (self.subject_code,self.subject_name)
