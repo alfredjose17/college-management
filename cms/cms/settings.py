ad081c83dc330700c8ed67f8c091bc53665ce638
akshayca1080 <akshayca99@gmail.com>
2020-07-28 18:50:26 +0530

    app name removed

--- cms/cms/settings.py
+++ cms/cms/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'cms.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'template/activitypoint')],
+        'DIRS': [os.path.join(BASE_DIR,'template')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
