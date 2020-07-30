fa5cb7fa19d098e0082e1862ee94e5747a0aa29d
Alfred V Jose <alfredzeee@gmail.com>
2020-07-30 19:45:00 +0530

    Adding the main template

--- cms/cms/settings.py
+++ cms/cms/settings.py
@@ -131,6 +131,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
-MEDIA_ROOT=os.path.join(BASE_DIR,'media')
-MEDIA_URL='/media/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'cms/static')]
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_URL = '/media/'
