19058f5889da85611ea2fc946fda57b2303755a9
Alfred Varghese Jose <alfredzeee@gmail.com>
2020-08-02 21:52:35 +0530

    Adding the main template (#14)

--- cms/cms/settings.py
+++ cms/cms/settings.py
@@ -141,8 +141,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 #url to redirect to after successful login and logout
-#LOGIN_REDIRECT_URL = ''
-#LOGOUT_REDIRECT_URL = ''
+LOGIN_REDIRECT_URL = '/home'
+LOGOUT_REDIRECT_URL = '/'
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'cms/static')]
