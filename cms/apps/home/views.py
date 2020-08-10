e8e4ad00ae040da1da775ac4c036bbe75059473e
Alfred V Jose <alfredzeee@gmail.com>
2020-08-10 15:06:49 +0530

    Fixing the navigation

--- cms/apps/home/views.py
+++ cms/apps/home/views.py
@@ -8,22 +8,33 @@ def home(request):
         if request.method == "POST":
             name = request.POST["class"] 
             classx = get_object_or_404(Internal, teacher = request.user, class_room__name = name)
-            #print(classx.class_room.id, classx.subject.id)
             context = {
                 'classes' : Internal.objects.filter(teacher = request.user),
                 'selected' : classx.class_room,
                 'type' : request.user.type,
                 'clasid' : classx.class_room.id,
-                'subid' : classx.subject.id
+                'subid' : classx.subject.id,
+                'classname' : request.user.class_name,
+                'fname' : request.user.first_name,
+                'lname' : request.user.last_name,
+                'roll' : request.user.rollno,
             }
         else:
             context = {
                 'classes' : Internal.objects.filter(teacher = request.user),
-                'type' : request.user.type
+                'type' : request.user.type,
+                'classname' : request.user.class_name,
+                'fname' : request.user.first_name,
+                'lname' : request.user.last_name,
+                'roll' : request.user.rollno,
             }
     else:
         context = {
-            'type' : request.user.type
+            'type' : request.user.type,
+            'fname' : request.user.first_name,
+            'lname' : request.user.last_name,
+            'classname' : request.user.class_name,
+            'roll' : request.user.rollno,
         }
 
     return render(request, 'home/index.html', context)
